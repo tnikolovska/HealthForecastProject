edff9d5d42b0a13d2d039b5d28074f0d4c0e635d
Teodora <nikolovskat95@gmail.com>
2023-02-10 21:46:48 +0100

    Fourth commit

--- src/main/java/com/teodora/springcloud/controllers/CategoryRestController.java
+++ src/main/java/com/teodora/springcloud/controllers/CategoryRestController.java
@@ -1,20 +1,32 @@
 package com.teodora.springcloud.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.teodora.springcloud.model.Category;
 import com.teodora.springcloud.repos.CategoryRepo;
 
 
-@RestController
-@RequestMapping("/cateogoryapi")
+@Controller
+//@RequestMapping("/cateogoryapi")
 public class CategoryRestController {
 	
 	@Autowired
@@ -30,6 +42,12 @@ public class CategoryRestController {
 		return repo.findByName(name);
 		
 	}
+	@RequestMapping(value="/categories/{id}", method = RequestMethod.GET)
+	public Category getCategory(@PathVariable("id") Long id) {
+		return repo.getReferenceById(id);
+		
+	}
+	
 	@RequestMapping(value="/updatecategories", method = RequestMethod.PUT)
 	public Category updateCategory(@RequestBody Category category) {
 		return repo.save(category);
@@ -44,4 +62,39 @@ public class CategoryRestController {
 		return repo.findAll();
 		
 	}
+	@RequestMapping(value="category",method = RequestMethod.GET)
+	public String detailsCategory(@RequestParam String name, Model model) {
+		Category category = getCategory(name);
+		model.addAttribute("name", category.getName());
+		model.addAttribute("beginRange", category.getBeginRange());
+		model.addAttribute("endRange",category.getEndRange());
+		return "category";
+		
+	}
+	//@RequestMapping(value="category-list",method = RequestMethod.GET)
+	@GetMapping("category-list")
+	public String categoryNames(Model model) {
+		List<Category> categories = new ArrayList<>();
+		categories=getCategories();
+		//List<String> names=categories.stream().map(Category::getName).collect(Collectors.toList());
+		model.addAttribute("categories",categories);
+		return "category-list";
+	}
+	@GetMapping("/edit/{id}")
+	public String showUpdateForm(@PathVariable("id")Long id,Model model) {
+		Category updateCategory = getCategory(id);
+		model.addAttribute("category",updateCategory);
+		return "update-category";
+	}
+	@PostMapping("/update/{id}")
+	public String updateCategory(@PathVariable("id") Long id,@Validated Category category,BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			category.setId(id);
+			return "update-category";
+		}
+		updateCategory(category);
+		return "redirect:/category-list";
+	}
+	
+	
 }	
